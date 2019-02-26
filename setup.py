46206587c41d680e0ae1e5a5f33e7ded320fd966
Alexander Böhn <fish2000@gmail.com>
2019-02-26 02:38:49 -0500

    Adding “long_description” to setup() call in setup.py ... also switched to using setuptools

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 
 import os
-from distutils.core import setup
+from setuptools import setup
 
 # PROJECT DIRECTORY
 PROJECT_NAME = 'Blit'
@@ -19,9 +19,25 @@ try:
 except:
     __version__ = '1.4.0'
 
+def project_content(filename):
+    import io
+    filepath = os.path.join(CWD, filename)
+    if not os.path.isfile(filepath):
+        raise IOError("""File %s doesn't exist""" % filepath)
+    out = ''
+    with io.open(filepath, 'r') as handle:
+        out += handle.read()
+    if not out:
+        raise ValueError("""File %s couldn't be read""" % filename)
+    return out
+
+LONG_DESCRIPTION = project_content('README.md')
+
 setup(name=PROJECT_NAME,
       version=__version__,
       description='Simple pixel-composition library.',
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type="text/markdown",
       author='Michal Migurski',
       author_email='mike@stamen.com',
       url='https://github.com/migurski/Blit',
